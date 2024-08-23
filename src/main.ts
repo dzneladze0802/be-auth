2a06d51a34350f4efdf0f1018b22fc54110b4b54
Davit Dzneladze <dzneladze0802@gmail.com>
2024-08-23 21:26:40 +0200

    add DB

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT);
+
+  console.log(`App started at port ${process.env.PORT}`);
 }
 bootstrap();
